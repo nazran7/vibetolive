5e153269dbf55a23304aba2ad5ea5b234dd1d454
yasir-okriya <yasir@okriya.com>
2025-10-13 12:02:49 +0600

    www used for cannonical issue for robots

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-    const baseUrl = 'https://vibetolive.dev'
+    const baseUrl = 'https://www.vibetolive.dev'
 
     return {
         rules: {
