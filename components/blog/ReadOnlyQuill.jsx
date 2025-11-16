5618d80066101724cff565d3be302b9d0c9610c7
yasir-okriya <yasir@okriya.com>
2025-11-16 10:12:53 +0600

    <ul> dark text in dark mood error solved

--- components/blog/ReadOnlyQuill.jsx
+++ components/blog/ReadOnlyQuill.jsx
@@ -7,7 +7,7 @@ const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 
 export default function ReadOnlyQuill({ value }) {
     return (
-        <div className="quill-readonly">
+        <div className="quill-readonly text-base-content">
             <ReactQuill theme="bubble" value={value || ""} readOnly modules={{ toolbar: false }} />
         </div>
     );
